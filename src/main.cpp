68dd8fbf3fdb48773094cd690af91186cf9f7aaf
Akin Sibay <sibayakin@gmail.com>
2020-10-07 17:26:07 +0300

    adding program support

--- src/main.cpp
+++ src/main.cpp
@@ -15,19 +15,25 @@
 #define ENABLE_PIN_C 33
 #define ENABLE_PIN_D 52
 #define ENABLE_PIN_E 53
-#define SET_ACCELERATION 200.0
+#define SET_ACCELERATION 300.0
 AccelStepper stepper(1, STEP_PIN_C, DIRECTION_PIN_C);
 float newSpeed;
-bool newDataBit = false;
+float maxSpeed = 3000.0;
+bool newDataBit, runAllowed = false,adding=false;
 char commandChar;
-long int steps = 0, newStep;
+long currentPosition;
+long int steps = 0, mainNewStep, addedNewStep,memMainStep;
 void checkSerial();
 void checkRunning();
+void mainProgram(long currentPositionValue,float maxSpeedValue,long stepValue);
+void addingProgram(long currentPositionValue,float maxSpeedValue,long stepValue);
+void stopMotor();
+void runMotor();
 void setup()
 {
   Serial.begin(9600);
   pinMode(ENABLE_PIN_C, OUTPUT);
-  digitalWrite(ENABLE_PIN_C, LOW);
+  digitalWrite(ENABLE_PIN_C, HIGH);
   stepper.setCurrentPosition(0);             //initial value
   stepper.setMaxSpeed(0.0);                  //initial value
   stepper.setAcceleration(SET_ACCELERATION); //initial value
@@ -40,48 +46,93 @@ void loop()
 }
 void checkRunning()
 {
-  if (stepper.distanceToGo() == 0)
-  {
-    digitalWrite(ENABLE_PIN_C, HIGH);
-    stepper.stop();
-    checkSerial();
+  if(!stepper.isRunning() && adding == true){ //İLAVE YAPMAYI BİTİRDİM
+    mainProgram(currentPosition,newSpeed,memMainStep);
   }
-  else
+  if (runAllowed == true) //B VEYA C KOMUTLARI GELMİŞ
   {
-    digitalWrite(ENABLE_PIN_C, LOW);
-    stepper.run();
-    checkSerial();
-  }
+    if (stepper.distanceToGo() == 0) //YOLU TAMAMLADIM
+    {
+      stopMotor();
+      checkSerial();
+    }
+    else //YOLU HENÜZ TAMAMLAMADIM
+    {
+      runMotor();
+      checkSerial();
+    }
+  }  
 }
 void checkSerial()
 {
   if (Serial.available())
   {
     newDataBit = true;
+    //B C D 
     commandChar = Serial.read();
-    newSpeed = Serial.parseFloat();
-    newStep = Serial.parseInt();
-    if (newDataBit == true)
-    {
-      if (stepper.distanceToGo() == 0)
-      {
-        steps = newStep;
-        stepper.setCurrentPosition(0.0);
-        stepper.setSpeed(0);
-        stepper.setMaxSpeed(newSpeed);
-        stepper.moveTo(steps);
-        Serial.println(stepper.currentPosition());
-      }
-      else
-      {
-        steps = newStep + steps;
-        stepper.setCurrentPosition(stepper.currentPosition());
-        stepper.setSpeed(0);
-        stepper.setMaxSpeed(newSpeed);
-        stepper.moveTo(steps);
-        Serial.println(stepper.currentPosition());
-      }
-      newDataBit = false;
-    }
   }
+  if (newDataBit == true) //YENİ VERİ GELDİ
+  {
+    newSpeed = Serial.parseFloat(); //HizTxt
+    mainNewStep = Serial.parseInt(); //ExtraStepAnaTxt
+    addedNewStep = Serial.parseInt(); //ExtraStepIlaveTxt
+    Serial.println(addedNewStep);
+    memMainStep = mainNewStep + memMainStep;
+    if (commandChar == 'B') //ANA PROGRAM
+    {
+      runAllowed = true;
+      adding = false; //İLAVE İŞLEMİ YAPMIYORUM
+      mainProgram(stepper.currentPosition(),newSpeed,mainNewStep); //memMainStep
+    }
+    if (commandChar == 'C') //İLAVE PROGRAM
+    {
+      runAllowed = true;
+      currentPosition = stepper.currentPosition(); //ANA PROGRAMA KALDIĞI YERDEN DEVAM ETSİN DİYE
+      addingProgram(0,maxSpeed,addedNewStep);
+      adding = true; //İLAVE İŞLEMİ YAPIYORUM   
+    }
+    if (commandChar == 'D') //DUR
+    {
+      runAllowed = false; 
+      stopMotor();
+    }
+    newDataBit = false;
+  }
+}
+
+void mainProgram(long currentPositionValue,float maxSpeedValue,long stepValue)
+{ 
+  if (stepper.distanceToGo() == 0) //YOLUMU TAMAMLADIM
+  {
+    adding = false; 
+    steps = stepValue;
+    stepper.setCurrentPosition(currentPositionValue);
+    //stepper.setSpeed(0);
+    stepper.setMaxSpeed(maxSpeedValue);
+    stepper.moveTo(steps);
+  }
+  else
+  {
+    steps = stepValue + steps;
+    stepper.setCurrentPosition(currentPositionValue);
+    //stepper.setSpeed(0);
+    stepper.setMaxSpeed(newSpeed);
+    stepper.moveTo(steps);
+  }
+}
+void addingProgram(long currentPositionValue,float maxSpeedValue,long stepValue){
+  stepper.setCurrentPosition(currentPositionValue);
+  stepper.setMaxSpeed(maxSpeedValue);
+  stepper.moveTo(stepValue); //move du
+}
+
+void runMotor(){
+  digitalWrite(ENABLE_PIN_C, LOW);
+  stepper.run();
+}
+void stopMotor(){
+  memMainStep = 0;
+  stepper.setCurrentPosition(0);
+  digitalWrite(ENABLE_PIN_C, HIGH);
+  stepper.stop();
 }
